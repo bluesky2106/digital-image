2bfa2c7f06327d8fbd1dd08abb4057765c0f24fa
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2021-04-06 16:22:19 +0700

    imwrite

---
+++ basic/opencv/imwrite.py
@@ -0,0 +1,22 @@
+# https://github.com/kyatou/python-opencv_tutorial/blob/master/08_image_encode_decode.py
+
+import cv2
+import utils
+
+# Load an color image in grayscale
+img = cv2.imread('./images/fpt.png', cv2.IMREAD_UNCHANGED)
+# cv2.imshow('original image',img)
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 95]
+result, encimg = cv2.imencode('.jpg', img, encode_param)
+if False==result:
+  print('could not encode image!')
+  quit()
+
+#decode from jpeg format
+decimg=cv2.imdecode(encimg,1)
+
+cv2.imshow('Source Image',img)
+cv2.imshow('Decoded image',decimg)
+
+utils.wait()
