7c0e923a15d4f5a10efaba1ef4fb6d785957a034
bluesky2106 <hoaiphuong.nguyen.vn.2106@gmail.com>
2021-04-06 16:22:13 +0700

    imread fix

--- basic/opencv/imread.py
+++ basic/opencv/imread.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import utils
 
 # Load an color image in grayscale
 img1 = cv2.imread('./images/fpt.png', cv2.IMREAD_UNCHANGED)
@@ -11,9 +12,4 @@ cv2.imshow('color image',img2)
 img3 = cv2.imread('./images/fpt.png', cv2.IMREAD_GRAYSCALE)
 cv2.imshow('gray image',img3)
 
-k = cv2.waitKey(0)
-if k == 27:         # wait for ESC key to exit
-  cv2.destroyAllWindows()
-elif k == ord('s'): # wait for 's' key to save and exit
-  cv2.imwrite('./resources/test.png',img)
-  cv2.destroyAllWindows()
+utils.wait()
